244bdf6553e038cca1a3a42be37c3416eef66cb8
Snblack <koracell@yandex.ru>
2020-08-03 16:42:28 +0300

    v2

--- app.rb
+++ app.rb
@@ -1,15 +1,14 @@
 class App
 
+  ALL_FORMATS = ['year', 'month', 'day', 'hour', 'min', 'sec']
+
   def call(env)
-    perfom_request
+    @path = env['PATH_INFO']
+    @query = env['QUERY_STRING']
     [status, headers, body]
   end
 
   private
-
-  def perfom_request
-    sleep rand(2..3)
-  end
 
   def status
     200
@@ -20,6 +19,27 @@ class App
   end
 
   def body
-    ["Welcome aboard\n"]
+    format = @query.gsub('format=', '').chomp('%').split('%2C')
+
+    time = Time.now
+    # Хеш с текущим временем
+    time_hash = {}
+
+    ALL_FORMATS.each do |form|
+      time_hash[form] = time.send(form)
+    end
+
+    time_output = []
+
+    format.each do |form|
+        time_output << time_hash[form] if time_hash.keys.include?(form)
+    end
+
+    [time_output.join('-')]
   end
 end
+
+# start_with?("format=")
+# /time?format=year%2Cmonth%2Cday
+# # "QUERY_STRING"=>"text=1:"
+# # "PATH_INFO"=>"/time:"
